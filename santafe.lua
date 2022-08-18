2507c4b207764bb009c6efb26db9dd5b92391823
Arii <69775542+Ryan-Levy@users.noreply.github.com>
2022-08-18 15:34:10 -0300

    Update santafe.lua

--- santafe.lua
+++ santafe.lua
@@ -1,29 +1,29 @@
 --- VARIAVEIS DE PERSONAGEM ---
 
-_G.plr = game:GetService("Players").LocalPlayer
-_G.TweenService = game:GetService("TweenService")
-_G.Part = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+local plr = game:GetService("Players").LocalPlayer
+local TweenService = game:GetService("TweenService")
+local Part = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
 
 --- VARIAVEIS DE LOCAL ---
 
-_G.Inicial = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(165.067642, 46.8500099, -231.402191)})
+local Inicial = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(165.067642, 46.8500099, -231.402191)})
 
-_G.Caminho1 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(248.964127, 47.9999924, 72.9024277)})
-_G.Caminho2 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(252.720383, 46.7999954, 588.220337)})
-_G.Caminho3 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(569.086914, 46.4738922, 752.250793)})
-_G.Caminho4 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(597.714478, 46.6738815, 1160.88159)})
-_G.Caminho5 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(715.029358, 46.4738922, 1457.61072)})
+local Caminho1 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(248.964127, 47.9999924, 72.9024277)})
+local Caminho2 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(252.720383, 46.7999954, 588.220337)})
+local Caminho3 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(569.086914, 46.4738922, 752.250793)})
+local Caminho4 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(597.714478, 46.6738815, 1160.88159)})
+local Caminho5 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(715.029358, 46.4738922, 1457.61072)})
 
-_G.Banco = TweenService:Create(Part, TweenInfo.new(1), {CFrame = CFrame.new(323.613647, 46.7999954, 122.810272)})
+local Banco = TweenService:Create(Part, TweenInfo.new(1), {CFrame = CFrame.new(323.613647, 46.7999954, 122.810272)})
 
-_G.Rojas1 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(881.208008, 74.6111832, 1445.24841)})
-_G.Rojas2 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(881.208008, 74.6111832, 1445.24841)})
+local Rojas1 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(881.208008, 74.6111832, 1445.24841)})
+local Rojas2 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(881.208008, 74.6111832, 1445.24841)})
 
-_G.Fita1 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(1102.28711, 46.6738968, 1117.70972)})
-_G.Fita2 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(1155.66345, 55.9502411, 1230.16406)})
+local Fita1 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(1102.28711, 46.6738968, 1117.70972)})
+local Fita2 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(1155.66345, 55.9502411, 1230.16406)})
 
-_G.Sucata1 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(1358.42407, 48.6346054, 1466.41968)})
-_G.Sucata2 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(1051.09534, 46.6739044, 1844.21729)})
+local Sucata1 = TweenService:Create(Part, TweenInfo.new(2), {CFrame = CFrame.new(1358.42407, 48.6346054, 1466.41968)})
+local Sucata2 = TweenService:Create(Part, TweenInfo.new(4), {CFrame = CFrame.new(1051.09534, 46.6739044, 1844.21729)})
 
 
 if FuncionarSpawn == true then
